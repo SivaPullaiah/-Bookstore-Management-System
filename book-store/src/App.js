6139ee1fbbdb080112d772d1a3dc17e6d316cc22
siva <mangalisivapullaiah@gmail.com>
2023-09-28 11:23:35 +0530

    just frontend login and signup  was added

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/Home";
 import AddBook from "./components/AddBook";
 import Books from "./components/Book/Books";
 import About from "./components/About";
 import BookDetail from "./components/Book/BookDetail";
+import Main from "./components/Main";
+import Signup from "./components/Singup";
+import Login from "./components/Login";
 function App() {
   return (
     <React.Fragment>
@@ -19,6 +22,10 @@ function App() {
           <Route path="/books" element={<Books />} />
           <Route path="/about" element={<About />} />
           <Route path="/books/:id" element={<BookDetail />} />
+          {<Route path="/" exact element={<Main />} />}
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/" element={<Navigate replace to="/login" />} />
         </Routes>
       </main>
     </React.Fragment>
--- book-store/src/components/Header.js
+++ book-store/src/components/Header.js
@@ -23,6 +23,7 @@ const Header = () => {
             <Tab LinkComponent={NavLink} to="/add" label="Add product" />
             <Tab LinkComponent={NavLink} to="/books" label="Books" />
             <Tab LinkComponent={NavLink} to="/about" label="About Us" />
+            <Tab LinkComponent={NavLink} to="/login" label="Login / Signup" />
           </Tabs>
         </Toolbar>
       </AppBar>
